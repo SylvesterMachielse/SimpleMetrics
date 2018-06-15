fd1d6611a46da06b2cdc6447d63b3a0281863084
Sylvester Machielse <sylvester.machielse@gmail.com>
2018-06-15 15:36:32 +0200

    Moved simplemetrics folder down and added a test project

---
+++ src/SimpleMetricsTest/SimpleMetricsTest/Controllers/MetricsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using SimpleMetrics.Contracts;
+
+namespace SimpleMetricsTest.Controllers
+{
+    public class MetricsController : Controller
+    {
+        private readonly IProvideMetricSnapshots _metricSnapshotProvider;
+
+        public MetricsController(IProvideMetricSnapshots metricSnapshotProvider)
+        {
+            _metricSnapshotProvider = metricSnapshotProvider;
+        }
+
+        [Route("metrics")]
+        [HttpGet]
+        public string Get()
+        {
+            var snapshot = _metricSnapshotProvider.Provide();
+
+            return snapshot;
+        }
+    }
+}
---
+++ src/SimpleMetricsTest/SimpleMetricsTest/Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SimpleMetrics.Contracts;
+using SimpleMetrics.Contracts.Measuring;
+
+namespace SimpleMetricsTest.Controllers
+{
+    [Route("api/[controller]")]
+    
+    public class ValuesController : Controller
+    {
+        private readonly IIncrementMetricsCounters _metricsCounterIncrementer;
+
+        public ValuesController(IIncrementMetricsCounters metricsCounterIncrementer)
+        {
+            _metricsCounterIncrementer = metricsCounterIncrementer;
+        }
+        
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _metricsCounterIncrementer.Increment(new ValuesModel());
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class ValuesModel : IMetricsModel
+    {
+        public ValuesModel()
+        {
+            this.Tags = new Dictionary<string, string>();
+        }
+
+        public string Namespace => "test_application";
+        public string ThingBeingMeasured => "valuescontroller";
+        public string Unit { get; }
+        public string Suffix => "total";
+        public Dictionary<string, string> Tags { get; }
+    }
+}
